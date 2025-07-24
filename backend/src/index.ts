a01e0757e63bcd60b19a5f30f4cc13f050a8f634
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-07-23 23:51:14 -0400

    Schema Para Database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-
+import colors from 'colors'
 import server from './server';
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log('Server is running on port:', port);
+  console.log(colors.blue.bold('Server is running on port:'), port);
 });
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -2,12 +2,9 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello from the router!')
-})
-
-router.get('/nosotros', (req, res) => {
-  res.send('Hello from the Nosotros!')
-})
+/** AUTENTICACION */
+router.post('/auth/register', (req, res) => {
+  console.log(req.body);
+});
 
 export default router;
